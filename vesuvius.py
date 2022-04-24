27c3d32fb5e590ada1e2685f98f0564c25c6dc3c
lonaslee <93890846+lonaslee@users.noreply.github.com>
2022-04-24 10:52:03 -0500

    Update vesuvius.py

--- vesuvius.py
+++ vesuvius.py
@@ -7,7 +7,12 @@ from discord.ext import commands
 
 
 def main():
-    logging.basicConfig(level=logging.WARNING)
+
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename='discord_log.log', encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
 
     d = {}
     for c in (65, 97):
@@ -29,6 +34,20 @@ def main():
         print(f'LOGGED ON: as {bot.user}')
         print(f'AT: {datetime.now().strftime("%m/%d/%y, %H:%M:%S")}')
         print('IN GUILDS:', ', '.join([g.name for g in bot.guilds]))
+
+    @bot.event
+    async def on_guild_join(guild):
+        print(f'ADDED to guild {guild.name}')
+
+    @bot.command(name='reload')
+    @commands.is_owner()
+    async def reload(ctx: commands.Context):
+        await ctx.send('reloading.')
+        print('RELOADING')
+        bot.reload_extension('vsvs_commands')
+        bot.reload_extension('vsvs_events')
+        bot.reload_extension('vsvs_features')
+        bot.reload_extension('vsvs_testing')
 
     bot.run(token)
 
