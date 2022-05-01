9c849b3973e159bf7fe0dea3586cedfbc3e05056
lonaslee <93890846+lonaslee@users.noreply.github.com>
2022-05-01 18:38:33 -0500

    Update vesuvius.py

--- vesuvius.py
+++ vesuvius.py
@@ -1,56 +1,72 @@
 import asyncio
 import discord
 import logging
+import aiosqlite
 import vsvs_config
 from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
 from discord.ext import commands
 
 
-def main():
-
-    logger = logging.getLogger('discord')
-    logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler(filename='discord_log.log', encoding='utf-8', mode='w')
-    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-    logger.addHandler(handler)
-
+async def main():
     d = {}
     for c in (65, 97):
         for i in range(26):
             d[chr(i + c)] = chr((i + 13) % 26 + c)
-    token = ''.join([d.get(c, c) for c in vsvs_config.token])
+    token = ''.join([d.get(c, c) for c in vsvs_config.tokenc])
+    bot = Bot()
+    async with bot:
+        await bot.start(token)
 
-    bot_intents = discord.Intents.default()
-    bot_intents.members = True
 
-    bot = commands.Bot(command_prefix='`', intents=bot_intents, help_command=None)
-    bot.load_extension('vsvs_commands')
-    bot.load_extension('vsvs_events')
-    bot.load_extension('vsvs_features')
-    bot.load_extension('vsvs_testing')
+class Bot(commands.Bot):
+    def __init__(self) -> None:
+        bot_intents = discord.Intents.all()
+        super().__init__(command_prefix='`', help_command=None, intents=bot_intents)
+        self.database = None
+        self._db_cursor = None
+        self._db_connnection = None
+        self._tpexecutor = ThreadPoolExecutor(1)
 
-    @bot.event
-    async def on_ready():
-        print(f'LOGGED ON: as {bot.user}')
+    async def run_in_tpexec(self, func):
+        return await asyncio.get_event_loop().run_in_executor(self._tpexecutor, func)
+
+    async def setup_hook(self) -> None:
+        await super().setup_hook()
+        await self.load_extension('vsvs_commands')
+        await self.load_extension('vsvs_events')
+        await self.load_extension('vsvs_features')
+        await self.load_extension('vsvs_games')
+        await self.load_extension('vsvs_testing')
+
+    async def start(self, *args, **kwargs):
+        async with aiosqlite.connect('vsvs_files/vsvs_database.db') as conn:
+            self._db_connnection = conn
+            self._db_cursor = await self._db_connnection.cursor()
+            self.database = vsvs_config.Database(self._db_connnection, self._db_cursor)
+            await self.database.create_games_table()
+            await self.database.create_wotd_table()
+            print("DATABASE connected with", self.database)
+            await super().start(*args, **kwargs)
+
+    async def on_ready(self):
+        print(f'LOGGED ON: as {self.user}')
         print(f'AT: {datetime.now().strftime("%m/%d/%y, %H:%M:%S")}')
-        print('IN GUILDS:', ', '.join([g.name for g in bot.guilds]))
+        print('IN GUILDS:', ', '.join([g.name for g in self.guilds]))
 
-    @bot.event
-    async def on_guild_join(guild):
+    async def on_guild_join(self, guild):
         print(f'ADDED to guild {guild.name}')
-
-    @bot.command(name='reload')
-    @commands.is_owner()
-    async def reload(ctx: commands.Context):
-        await ctx.send('reloading.')
-        print('RELOADING')
-        bot.reload_extension('vsvs_commands')
-        bot.reload_extension('vsvs_events')
-        bot.reload_extension('vsvs_features')
-        bot.reload_extension('vsvs_testing')
-
-    bot.run(token)
 
 
 if __name__ == '__main__':
-    main()
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(
+        filename='vsvs_files/discord_log.log', encoding='utf-8', mode='w'
+    )
+    handler.setFormatter(
+        logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+    )
+    logger.addHandler(handler)
+
+    asyncio.run(main())
