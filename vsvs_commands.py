35ef52ce6ff91f316e935fb75ad82f22692ce89b
lonaslee <93890846+lonaslee@users.noreply.github.com>
2022-05-01 18:37:38 -0500

    Update vsvs_commands.py

--- vsvs_commands.py
+++ vsvs_commands.py
@@ -21,22 +21,29 @@ class OwnerCommands(commands.Cog):
             return e.author == ctx.author and e.channel == ctx.channel
 
         try:
-            confirmation = await self.bot.wait_for(
-                event='message', check=chk, timeout=10
-            )
+            confirmation = await self.bot.wait_for('message', check=chk, timeout=10)
         except asyncio.TimeoutError:
             await ctx.send('timed out')
             return
         if confirmation.content == 'yes':
+            print("EXITING on exit command")
             await ctx.send('good bye!')
             await self.bot.close()
 
-    @exit.error
-    async def exit_error(self, ctx: commands.Context, error: commands.CommandError):
-        if isinstance(error, commands.NotOwner):
-            await ctx.send('permission denied.')
-        else:
-            await ctx.send(error)
+    @commands.command(name='dbexec')
+    @commands.is_owner()
+    async def dbexec(self, ctx: commands.Context, query: str):
+        await self.bot.database.cursor.execute(query)
+
+    @commands.command(name='reload')
+    @commands.is_owner()
+    async def reload(self, ctx: commands.Context):
+        await ctx.send('reloading.')
+        print('RELOADING extensions')
+        await self.bot.reload_extension('vsvs_events')
+        await self.bot.reload_extension('vsvs_features')
+        await self.bot.reload_extension('vsvs_testing')
+        await self.bot.reload_extension('vsvs_commands')
 
     @commands.command(name='setstatus')
     @commands.cooldown(rate=1, per=30, type=commands.BucketType.default)
@@ -65,14 +72,15 @@ class OwnerCommands(commands.Cog):
     async def setactivity(
         self, ctx: commands.Context, type: str, *, act: str = 'something'
     ):
-        """set the activity of the bot. usage: `setactivity {playing, streaming, watching, listening, competing} {...}"""
+        """set the activity of the bot. usage: `setactivity {playing, streaming, watching, listening, competing} ..."""
         sts_rn = ctx.me.status
         match type := type.lower():
             case 'playing':
                 new_act = discord.Game(act)
             case 'streaming':
-                stream = act.split(' ', maxsplit=1)
+                stream = act.rsplit(' ', maxsplit=1)
                 new_act = discord.Streaming(name=stream[0], url=stream[1])
+                sts_rn = discord.Status.online
             case 'watching':
                 new_act = discord.Activity(type=discord.ActivityType.watching, name=act)
             case 'listening':
@@ -90,7 +98,7 @@ class OwnerCommands(commands.Cog):
             case _:
                 raise commands.BadArgument(
                     message='invalid activity. must be of types '
-                    '{playing, streaming, watching, listening, competing}{...}'
+                    '{playing, streaming, watching, listening, competing} {...}'
                 )
         await self.bot.change_presence(activity=new_act, status=sts_rn)
         await ctx.send(
@@ -98,6 +106,28 @@ class OwnerCommands(commands.Cog):
             f'{"to " if type == "listening" else "in " if type == "competing" else ""}'
             f'{new_act.name}\"'
         )
+
+    @commands.command(name='sendtf')
+    @commands.is_owner()
+    async def send_tf(self, ctx: commands.Context, channel: int):
+        def chk(m):
+            return m.author == ctx.author and m.channel == ctx.channel
+
+        channel = self.bot.get_channel(channel)
+        await ctx.send(
+            f'are you sure you want to send <:tf:968279073367400458> '
+            f'to channel  "{channel.name}"  in guild  "{channel.guild.name}" ?'
+        )
+        try:
+            m = await self.bot.wait_for('message', check=chk, timeout=10)
+        except asyncio.TimeoutError:
+            await ctx.send('tfsend cancelled.')
+            return
+        if m.content == 'yes':
+            await channel.send('<:tf:968279073367400458>')
+            await ctx.send('sent!  <:tf:968279073367400458>')
+        else:
+            await ctx.send('ok.  <:td:968282301593157723>')
 
 
 class GeneralCommands(commands.Cog):
@@ -130,7 +160,8 @@ class GeneralCommands(commands.Cog):
     async def default_help(self) -> discord.Embed:
         embed_msg = discord.Embed(
             title='Vesuvius#8475',
-            description='small bot that will have more stuff added gradually',
+            description='small bot that will have more stuff added gradually\n'
+            '```\n`help [command]\n[]: optional arguments, {}: options```',
             colour=discord.Colour.dark_orange(),
         )
         cmds = '\n\`'.join(
@@ -141,6 +172,7 @@ class GeneralCommands(commands.Cog):
                 'help',
                 'starttime',
                 'ping',
+                'leaderboard',
                 'snipe',
                 'source',
             ]
@@ -202,10 +234,67 @@ class GeneralCommands(commands.Cog):
         diff = end - start
         await ctx.send(f'API:     {round(diff * 1000)}ms.')
 
+    @commands.command(name='wins', aliases=['leaderboard'])
+    @commands.cooldown(rate=1, per=180, type=commands.BucketType.channel)
+    async def wins(self, ctx: commands.Context):
+        """display the leaderboard for wins in games! usage: `leaderboard"""
+        medals = {0: '\u001b[1;33m', 1: '\u001b[1;30m', 2: '\u001b[1;31m'}
+        ebd = discord.Embed(
+            title='Game Wins, Losses, and Ties!',
+            description=None,
+            colour=discord.Colour.dark_green(),
+        )
+        lst = await self.bot.database.get_games_winloss()
+        lst.sort(
+            key=(lambda v: v[1] + v[4] + v[7] + v[10] - v[2] - v[5] - v[8] - v[11]),
+            reverse=True,
+        )
+        fs = '\u001b[0m\u001b[4;37m\u001b[1;37m       ttt | c4    | rvsi  | weiqi '
+        for k, r in enumerate(lst):
+            pairs = ' | '.join(
+                [
+                    f'{r[1]}:{r[2]}:{r[3]}',
+                    f'{r[4]}:{r[5]}:{r[6]}',
+                    f'{r[7]}:{r[8]}:{r[9]}',
+                    f'{r[10]}:{r[11]}:{r[12]}',
+                ]
+            )
+            dis_name = ctx.guild.get_member(r[0]).display_name
+            if len(dis_name) > 20:
+                dis_name = dis_name[:12] + '...'
+            color = medals.get(k, '\u001b[0;32m')
+            fs = ''.join(
+                ['\n', fs, '\n', color, f'#{k + 1} - ', pairs, ' - ', dis_name]
+            )
+        ebd.add_field(name='--- Leaderboard ---', value=f'```ansi{fs}```')
+        await ctx.send(embed=ebd)
+
+    @commands.command(name='sendmsg')
+    @commands.is_owner()
+    async def sendmsg(self, ctx: commands.Context, channel: int, *, msg: str):
+        def chk(m):
+            return m.author == ctx.author and m.channel == ctx.channel
+
+        channel = self.bot.get_channel(channel)
+        await ctx.send(
+            f'are you sure you want to send  "{msg}"  '
+            f'to channel  "{channel.name}"  in guild  "{channel.guild.name}" ?'
+        )
+        try:
+            m = await self.bot.wait_for('message', check=chk, timeout=10)
+        except asyncio.TimeoutError:
+            await ctx.send('tfsend cancelled.')
+            return
+        if m.content == 'yes':
+            await channel.send(msg)
+            await ctx.send('sent!')
+        else:
+            await ctx.send('ok.')
+
     @commands.command(name='c4')
-    @commands.cooldown(rate=1, per=60)
+    @commands.cooldown(rate=1, per=60, type=commands.BucketType.user)
     async def c4(self, ctx: commands.Context, *, fillers=None):
-        """suprise for people who use `c4 as a shorthand for `connectfour"""
+        """suprise for people who use \`c4 as a shorthand for \`connectfour"""
         await ctx.message.reply('bomb has been planted!')
         for n in reversed(range(1, 6)):
             await ctx.send(f'{n}...')
@@ -213,7 +302,7 @@ class GeneralCommands(commands.Cog):
         await ctx.message.reply('BOOM! :boom:')
 
 
-def setup(bot: commands.Bot):
-    bot.add_cog(OwnerCommands(bot))
-    bot.add_cog(GeneralCommands(bot))
+async def setup(bot: commands.Bot):
+    await bot.add_cog(OwnerCommands(bot))
+    await bot.add_cog(GeneralCommands(bot))
     print('LOADED commands')
